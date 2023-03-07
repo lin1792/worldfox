a155527b95c8e3fd07ecce93709a3ee417afebbf
lin1792 <1792048892@qq.com>
2023-03-07 14:01:41 +0800

    modified scroll

--- src/layout/IndexView.vue
+++ src/layout/IndexView.vue
@@ -5,7 +5,7 @@ import NavbarView from './components/NavbarView.vue';
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative overflow-hidden">
   <NavbarView class="fixed top-0 left-0 w-screen h-16"></NavbarView>
   
   <AppmainViewVue class="w-screen h-screen pt-16 bg-cover bg-homeImgae"></AppmainViewVue>
